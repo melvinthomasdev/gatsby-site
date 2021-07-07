bacd08fcbb3bdd15160087b97033951b57627c55
Melvin Thomas <melvinchooranolil@gmail.com>
2021-07-08 01:51:35 +0530

    basic

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+const aboutPage = () => {
+    return (
+        <main>
+            <title>
+                About Me
+            </title>
+            <h1> About Me</h1>
+            <p>Hi, I'm the proud creatoe of this website</p>
+        </main>
+
+    )
+}
+
+export default aboutPage
