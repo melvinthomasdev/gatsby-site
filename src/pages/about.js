50695ffabb8cdf650d2e48ded4c3dd7d00c0d3a3
Melvin Thomas <melvinchooranolil@gmail.com>
2021-07-24 08:02:48 +0530

    tut p2 Done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,20 @@
 import * as React from 'react'
+// import { Link } from 'gatsby'
+import Layout from '../componenets/layout'
 
 const aboutPage = () => {
     return (
-        <main>
-            <title>
-                About Me
-            </title>
-            <h1> About Me</h1>
-            <p>Hi, I'm the proud creatoe of this website</p>
-        </main>
+        // <main>
+        //     <title>
+        //         About Me
+        //     </title>
+        //     <h1> About Me</h1>
+        //     <Link to="/">Back to home</Link>
+        //     <p>Hi, I'm the proud creatoe of this website</p>
+        // </main>
+        <Layout pageTitle="About Me">
+            <p>Hey  There! I am the creator of this site and I buit it using Gatsby</p>
+        </Layout>
 
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const indexPage = () => {
   return (
     <main>
       <title>Homepage</title>
       <h1>Welcom to my Gatsby Site</h1>
+      <Link to="/about">About</Link>
       <p>I;m making this by following the Gatsby tutorial</p>
     </main>
   )
